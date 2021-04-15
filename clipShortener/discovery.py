90b15c3980b5a4a018aa68f5027b01bc15408740
Hazel <hazelrella11@gmail.com>
2021-04-14 22:24:50 -0700

    Commented discoverer

--- clipShortener/discovery.py
+++ clipShortener/discovery.py
@@ -12,10 +12,17 @@ logger = getLogger('clipShortener')
 
 @dataclass(frozen=True)
 class Discoverer:
+    """A representation of parameters used to discover files."""
+
     fileTypes: Optional[List[str]] = None
     minimumSize: Optional[int] = None
 
     def discover(self, directory: Optional[str] = None) -> List[str]:
+        """
+        Iterate through the files in a given directory and returns all valid files.
+
+        If directory is None then uses the current working directory.
+        """
         if directory is not None:
             path = Path(directory)
             logger.log(2, f'[~] Created path at {directory}.')
@@ -23,19 +30,21 @@ class Discoverer:
             path = Path.cwd()
             logger.log(2, f'[~] Created path in current working directory.')
 
-        if not path.exists() or path.is_file():
+        if not path.exists() or path.is_file():  # if the directory path given is invalid
             return []
 
         files = []
 
         for child in path.iterdir():
             if child.is_file():
+                # File type check
                 if self.fileTypes is not None and child.suffix not in self.fileTypes:
                     logger.log(1, f'[-] Disqualified "{child.name}" ({child.suffix} != [{", ".join(self.fileTypes)}]).')
                     continue
 
                 stat = child.stat()
 
+                # Minimum size check
                 if self.minimumSize is not None and stat.st_size < self.minimumSize:
                     logger.log(1, f'[-] Disqualified "{child.name}" ({stat.st_size} < {self.minimumSize}).')
                     continue
