61a8320a51c4467cd40f7a1222709f225120deee
Hazel <hazelrella11@gmail.com>
2021-04-14 17:31:48 -0700

    Set up basic package functionality and readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# BasePythonPackage
-Basic python package template
+# Clip Shortener
+Clips are big, compress all of them at once with FFMPEG.
+
+# Installation
+1. Clone this repo.
+2. While in the directory, run `pip install .`.
+3. Install [FFmpeg](https://www.ffmpeg.org).
+
+# Usage
+Go to the directory with clips and run `python3 -m clipShortener -h` to view help.
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-packageName = 'base-package'
+packageName = 'clip-shortener'
 
 major = 0
 minor = 0
@@ -8,14 +8,14 @@ micro = 1
 
 pythonVersion = '3.6'
 
-with open('description.md', 'r') as longDescription:
+with open('README.md', 'r') as longDescription:
     description = longDescription.read()
 
 setuptools.setup(
     name=f'{packageName}-hazel-trinity',
     version=f'{major}.{minor}.{micro}',
     author='Hazel Trinity',
-    description='Base package description.',
+    description='Shortens clips into a small .',
     long_description=description,
     long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
@@ -24,5 +24,9 @@ setuptools.setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent'
     ],
-    python_requires=f'>={pythonVersion}'
+    python_requires=f'>={pythonVersion}',
+    install_requires=[
+        'click',
+        'ffmpeg-python',
+    ],
 )
